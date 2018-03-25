672cf5b0aa6c7086814bdf104a6e89845240b568
Augene Park <augenepark@Augenes-MacBook-Air.local>
2018-03-25 03:35:42 -0400

    action op

--- action.cpp
+++ action.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 using namespace cv;
 
-void draw( Mat& img, double scale, bool drawRect );
+void draw( Mat& img, double scale, bool drawRect);
 
 int main( int argc, const char** argv ){
     // VideoCapture class for playing video for which faces to be detected
@@ -19,6 +19,7 @@ int main( int argc, const char** argv ){
 
     // Start Video..1) 0 for WebCam 2) "Path to Video" for a Local Video
     bool webcam = true;
+    double ACTION_THRESH = 75000000;
     if(!webcam)
         capture.open("test_vids/videoplayback.mp4"); 
     else
@@ -33,7 +34,8 @@ int main( int argc, const char** argv ){
             if( frame.empty() )
                 break;
 
-            if(!first){
+            //cout << frame.rows << " " << lastFrame.rows << endl;
+            if(!first && frame.cols == lastFrame.cols && frame.rows == lastFrame.rows){
                 absdiff(lastFrame,frame,diff);
                 actionCoefficient = 0;
                 for(i=0;i<diff.rows;i++){
@@ -44,11 +46,13 @@ int main( int argc, const char** argv ){
                         actionCoefficient += dist;
                     }
                 }
-                cout << actionCoefficient << endl;
+                //cout << actionCoefficient << endl;
             }
 
+            lastFrame = frame;
+
             if(webcam){
-                draw(frame,scale,actionCoefficient>1000000);
+                draw(frame,scale,actionCoefficient > ACTION_THRESH);
             }
             
             char c = (char)waitKey(10);
@@ -57,7 +61,6 @@ int main( int argc, const char** argv ){
             if( c == 27 || c == 'q' || c == 'Q' ) 
                 break;
             first = false;
-            lastFrame = frame;
         }
     }
     else
@@ -67,8 +70,9 @@ int main( int argc, const char** argv ){
 
 void draw( Mat& img,
         double scale, bool drawRect){
-    Scalar color = Scalar(255,0,0);
-    rectangle(img,cvPoint(0,0),cvPoint(cvRound((img.cols-1)*scale),cvRound((img.rows-1)*scale)),color,3,8,0);
+    Scalar color = Scalar(0,0,255);
+    if(drawRect)
+        rectangle(img,cvPoint(0,0),cvPoint(cvRound(img.cols*scale),cvRound(img.rows*scale)),color,20,8,0);
     resize(img, img, Size(640, 360), 0, 0, INTER_CUBIC);
     imshow( "Webcam", img ); 
 }
